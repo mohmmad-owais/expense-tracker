82e258eb6f24f2c3eb6232735441a36891f000fa
Owais <precisework01@gmail.com>
2020-11-25 23:15:56 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
+
 
 function App() {
   return (
-    <div >
-      Hello World
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+        
+      </div>
+    </GlobalProvider>
   );
 }
 
